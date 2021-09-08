76d50abfbcb6d5c501b94922842950437ae52fef
Sundardass <sundarkalpana56@gmail.com>
2021-09-08 08:58:42 +0530

    username

--- src/main/java/projsundar/login.java
+++ src/main/java/projsundar/login.java
@@ -1,5 +1,10 @@
 package projsundar;
 
 public class login {
-
+      public static void username() {
+System.out.println("user name");
+	}
+      public static void main(String[] args) {
+		username();
+	}
 }
